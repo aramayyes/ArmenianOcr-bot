bec8ed110df924f23aa9bb7c609d357638c539e7
aramayyes <aramayis.amiraghyan@outlook.com>
2021-03-07 17:50:53 +0400

    Change polling to webhook.

--- src/bot.py
+++ src/bot.py
@@ -6,7 +6,7 @@ import pytesseract
 from PIL import Image
 from aiogram import Bot, Dispatcher, executor, types
 
-from config import BotConfig
+from config import BotConfig, WebAppConfig
 from response_msgs import ResponseMsgs
 
 # Configure logging
@@ -16,6 +16,20 @@ logging.basicConfig(level=logging.INFO)
 loop = asyncio.get_event_loop()
 bot = Bot(token=BotConfig.API_TOKEN, loop=loop)
 dp = Dispatcher(bot)
+
+
+async def on_startup(dp):
+    """
+    Called on bot startup.
+    """
+    await bot.set_webhook(BotConfig.WEBHOOK_URL)
+
+
+async def on_shutdown(dp):
+    """
+    Called on bot shutdown.
+    """
+    await bot.delete_webhook()
 
 
 @dp.message_handler(content_types=['photo'])
@@ -60,4 +74,5 @@ def image_to_string(image, lang='hye'):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_webhook(dp, '/', on_startup=on_startup, on_shutdown=on_shutdown, host='0.0.0.0',
+                           port=WebAppConfig.PORT)
